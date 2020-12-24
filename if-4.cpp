cf59a4c553b238fc8097303880adbecf00ca4dee
alenahalm <alenahalmatova749@gmail.com>
2020-12-24 14:02:20 +0800

    Initial commit

---
+++ if-4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double v1, p1;
+	cout << "Enter velocity and price of a taxi" << endl;
+	cin >> v1 >> p1;
+	double v2;
+	cout << "Enter person's velocity" << endl;
+	cin >> v2;
+	double s, t;
+	cout << "Enter needed distance and time" << endl;
+	cin >> s >> t;
+	if (s / v2 <= t) {
+		cout << "Go by foot" << endl;
+	}
+	else if (s / v1 <= t) {
+		cout << "Take a taxi for " << s * p1 << endl;
+	}
+	else {
+		cout << "It's impossible" << endl;
+	}
+	system("pause");
+}
